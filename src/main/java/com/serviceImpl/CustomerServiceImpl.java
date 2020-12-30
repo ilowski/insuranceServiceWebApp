6c9e8401032f0dc604ed9fed51ce203a2098242e
ilowski <wojtek-ilowski@wp.pl>
2020-12-30 12:02:44 +0100

    implement interface CustomerService; create random list of customers; implement methods from CustomerService interface

---
+++ src/main/java/com/serviceImpl/CustomerServiceImpl.java
@@ -0,0 +1,41 @@
+package com.serviceImpl;
+
+import com.entity.Customer;
+import org.springframework.stereotype.Service;
+import com.service.CustomerService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    public static List<Customer> listCustomers = new ArrayList<>();
+
+    private static Long COUNTER = 1l;
+
+    static {
+        listCustomers.add(new Customer(COUNTER++, "Adam", "Boczek"));
+        listCustomers.add(new Customer(COUNTER++, "Magda", "Kozak"));
+        listCustomers.add(new Customer(COUNTER++, "Grazyna", "Zdziek"));
+        listCustomers.add(new Customer(COUNTER++, "Bartosz", "Kox"));
+    }
+
+    @Override
+    public List<Customer> findAllCustomers() {
+
+        return listCustomers;
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        for (Customer x : listCustomers) {
+            if (x.getId() == id) {
+                return x;
+            }
+        }
+        return null;
+
+    }
+}
