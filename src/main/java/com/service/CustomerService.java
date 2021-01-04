979504b1fb632bf984b31ac197c9c28af8901600
ilowski <wojtek-ilowski@wp.pl>
2021-01-04 13:11:58 +0100

    change findById to return Optional of Customers for jpa repository

--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -1,15 +1,17 @@
 package com.service;
 
 import com.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomerService {
 
-    public List<Customer> findAllCustomers();
+    public Page<Customer> findAllCustomers(Pageable pageable);
 
-    public Customer findById(Long id);
+    public Optional<Customer> findById(Long id);
 
     public void addCustomer(Customer customer);
 
