2fbfbc2dae5de4359c853d24ca3aaee4b99ac867
ilowski <wojtek-ilowski@wp.pl>
2021-01-06 20:43:45 +0100

    add new method findByFirstName and findBySecondName - both return list of customers

--- src/main/java/com/repository/CustomerRepository.java
+++ src/main/java/com/repository/CustomerRepository.java
@@ -4,7 +4,12 @@ import com.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface  CustomerRepository extends JpaRepository<Customer, Long> {
+
+    public List<Customer> findByFirstName(String firstName);
+    public List<Customer> findBySecondName(String secondName);
 
 }
