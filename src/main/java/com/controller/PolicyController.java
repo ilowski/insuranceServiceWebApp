5b8b7b9af07faa26b27a95acd1491465d88c34ed
ilowski <wojtek-ilowski@wp.pl>
2021-01-27 20:25:15 +0100

    first commit; Controller with mapping for add, update, delete, findall policy

---
+++ src/main/java/com/controller/PolicyController.java
@@ -0,0 +1,74 @@
+package com.controller;
+
+import com.entity.Customer;
+import com.entity.Policy;
+import com.service.PolicyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/policies")
+public class PolicyController {
+
+    @Autowired
+    private PolicyService policyService;
+
+    @GetMapping
+    public ResponseEntity<?> findAllPolicies(Pageable pageable) {
+        return new ResponseEntity<Page<Policy>>(policyService.findAllPolicies(pageable), HttpStatus.OK);
+    }
+    @GetMapping("/search")
+    public ResponseEntity<?> findByCriteria(@RequestParam(name = "criteria", required = true) String criteria,
+                                            @RequestParam(name = "searchItem", required = true) String searchItem) {
+        return new ResponseEntity<List<Policy>>(policyService.findByCriteria(criteria, searchItem),HttpStatus.OK);
+    }
+
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addPolicy(@RequestBody Policy policy) {
+        try{
+      policyService.addPolicy(policy);
+        return new ResponseEntity<Void>(HttpStatus.CREATED); }
+        catch (Exception e) {
+            return new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping("/{numberOfPolicy}")
+    public ResponseEntity<?> findByNumberOfPolicy(@PathVariable String numberOfPolicy) {
+        Optional<Policy> policyOptional = policyService.findByNumberOfPolicy(numberOfPolicy);
+        if (policyOptional.isPresent()) {
+            return new ResponseEntity<Policy>(policyOptional.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> removePolicy(@PathVariable String numberOfPolicy) {
+        if (policyService.removePolicy(numberOfPolicy)) {
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updatePolicy(@Valid @RequestBody Policy policy) {
+        if (policyService.updatePolicy(policy)) {
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+    }
+
+}
