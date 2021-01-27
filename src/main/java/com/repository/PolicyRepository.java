35c2af413854af2a18f29a40d67f7a2eec715162
ilowski <wojtek-ilowski@wp.pl>
2021-01-27 20:31:46 +0100

    first commit; add find methods

---
+++ src/main/java/com/repository/PolicyRepository.java
@@ -0,0 +1,20 @@
+package com.repository;
+
+import com.entity.Customer;
+import com.entity.Policy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PolicyRepository extends JpaRepository<Policy, Long> {
+    public Optional<Policy> findByNumberOfPolicy(String numberOfPolicy);
+    public List<Policy> findByInsuranceCompany(String insuranceCompany);
+    public List<Policy> findByCustomerId (Long customerId);
+    public List<Policy> findByTypeOfPolicy (String typeOfPolicy);
+    public List<Policy> findByDateOfStartPolicy (Date dateOfStartPolicy);
+    public List<Policy> findByDateOfEndPolicy (Date dateOfEndPolicy);
+}
