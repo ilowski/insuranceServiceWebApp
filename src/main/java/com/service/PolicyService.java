1e0bae12a7db1490716de5c180c697d26b8c6e90
ilowski <wojtek-ilowski@wp.pl>
2021-01-30 01:45:45 +0100

    add parseException to findByCriteria method

--- src/main/java/com/service/PolicyService.java
+++ src/main/java/com/service/PolicyService.java
@@ -5,6 +5,7 @@ import com.entity.Policy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +19,7 @@ public interface PolicyService {
 
     public void addPolicy(Policy policy) throws Exception;
 
-    public List<Policy> findByCriteria(String criteria, String searchItem);
+    public List<Policy> findByCriteria(String criteria, String searchItem) throws ParseException;
 
     public Boolean removePolicy(String numberOfPolicy);
 
