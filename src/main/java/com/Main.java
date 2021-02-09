52ff6c4402f1289f88c663e385a4d2a3894f4a47
ilowski <wojtek-ilowski@wp.pl>
2021-02-09 22:58:01 +0100

    add initialization of modelmapper

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -17,6 +18,12 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
     }
 
     @Bean
