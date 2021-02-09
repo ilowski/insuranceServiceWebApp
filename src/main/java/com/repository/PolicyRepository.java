da8fa66d081e138e35acfbce099aa95f7a94c2ca
ilowski <wojtek-ilowski@wp.pl>
2021-02-09 22:55:58 +0100

    change return of findBYNumberOfPolicy from Optional to Policy

--- src/main/java/com/repository/PolicyRepository.java
+++ src/main/java/com/repository/PolicyRepository.java
@@ -11,21 +11,15 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-    public Optional<Policy> findByNumberOfPolicy(String numberOfPolicy);
+public interface PolicyRepository extends JpaRepository<Policy, String> {
+    public Policy findByNumberOfPolicy(String numberOfPolicy);
 
     public List<Policy> findByInsuranceCompany(String insuranceCompany);
 
-    public List<Policy> findByCustomerId(Long customerId);
-
     public List<Policy> findByTypeOfPolicy(String typeOfPolicy);
 
-    public List<Policy> findByDateOfStartPolicy(Date dateOfStartPolicy);
-
-    @Query(value = "select * from Policies where datediff(curdate(),date_of_end_policy)>=14",
-    nativeQuery = true)
-    public List<Policy> findTwoWeeksPolicies();
+    public List<Policy> findByDateOfStartPolicy(String dateOfStartPolicy);
 
 
-    public List<Policy> findByDateOfEndPolicy(Date dateOfEndPolicy);
+    public List<Policy> findByDateOfEndPolicy(String dateOfEndPolicy);
 }
