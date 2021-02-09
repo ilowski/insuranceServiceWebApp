c3bdd65ea190598a9e748d2acd0c5931aa09bca9
ilowski <wojtek-ilowski@wp.pl>
2021-02-09 22:55:15 +0100

    update interface, add method findById and update method findByPesel to return Customer instead List<Customer>

--- src/main/java/com/repository/CustomerRepository.java
+++ src/main/java/com/repository/CustomerRepository.java
@@ -2,18 +2,23 @@ package com.repository;
 
 import com.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer>, CrudRepository<Customer,Long> {
 
     public List<Customer> findByFirstName(String firstName);
 
     public List<Customer> findBySecondName(String secondName);
 
-    public List<Customer> findByPesel(String pesel);
+    public Customer findByPesel(String pesel);
+
+    public Customer findById(long id);
+
 
 
 }
