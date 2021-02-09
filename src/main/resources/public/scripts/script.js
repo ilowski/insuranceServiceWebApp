d79fc2fbf186eca3f80e10750fa05e6526aa4073
ilowski <wojtek-ilowski@wp.pl>
2021-02-09 23:00:42 +0100

    change method of display data from server

--- src/main/resources/public/scripts/script.js
+++ src/main/resources/public/scripts/script.js
@@ -1,43 +1,79 @@
 CUSTOMERS_API_URL = 'http://localhost:8080/api/customers';
 POLICIES_API_URL = 'http://localhost:8080/api/policies';
 
-fetch(`${CUSTOMERS_API_URL}`)
-.then(response => response.json())
-.then(customers => {
-let out = '';
-    customers.forEach(customer => {
 
-        out+=`
+
+
+
+
+fetch(`${POLICIES_API_URL}`)
+    .then(response => response.json())
+    .then(policies => {
+        let out = '';
+        policies.forEach(policy => {
+
+            out += `
         <tr>
-        <td>${customer.firstName}</td>
-        <td>${customer.secondName}</td>
+        <th onclick="location.href='profile.html?pesel=${policy.customer.pesel}' " >${policy.customer.pesel} </th>
+        <th>${policy.customer.secondName}</th>
+        <th onclick="location.href='${POLICIES_API_URL}/${policy.numberOfPolicy}' " >${policy.numberOfPolicy}</th>
+        <th>${policy.typeOfPolicy}</th>
+        <th>${policy.insuranceCompany}</th>
+        <th>${policy.dateOfStartPolicy}</th>
+        <th>${policy.dateOfEndPolicy}</th>
+
+
         </tr>`
+        })
+        document.getElementById('tablebody').innerHTML += out;
     })
-    document.getElementById("policies").innerHTML += out;
-})
+
+document.getElementById('search').addEventListener('click', (event) => {
+    event.preventDefault();
+    let criteria = document.getElementById('criteria').value;
+    let searchItem = document.getElementById('searchItem').value; {
+        {
+            fetch(`${POLICIES_API_URL}/${searchItem}`)
+                .then(response => {
+                    if (!response.ok) {
+                        // Note: Strongly recommend using Error for exceptions/rejections
+                        throw new Error("HTTP error " + response.status);
+                    }
+                    return response.json();
+
+                })
+                .then(policy => {
+
+                    let out = '';
 
 
-
-
-
-fetch(`${POLICIES_API_URL}/searchTwoWeeksPolicies`)
-.then(response => response.json())
-.then(policies => {
-let out = '';
-    policies.forEach(policy=> {
-
-        out+=`
+                    out = `
         <tr>
-        <td>${policy.numberOfPolicy}</td>
-        <td>${policy.typeOfPolicy}</td>
-        <td>${policy.insuranceCompany}</td>
-        <td>${policy.dateOfStartPolicy}</td>
-        <td>${policy.dateOfEndPolicy}</td>
-        <td>${policy.customerId}</td>
+        <th onclick="location.href='profile.html?pesel=${policy.customer.pesel}' " >${policy.customer.pesel} </th>
+        <th>${policy.customer.secondName}</th>
+        <th onclick="location.href='${POLICIES_API_URL}/${policy.numberOfPolicy}' " >${policy.numberOfPolicy}</th>
+        <th>${policy.typeOfPolicy}</th>
+        <th>${policy.insuranceCompany}</th>
+        <th>${policy.dateOfStartPolicy}</th>
+        <th>${policy.dateOfEndPolicy}</th>
         </tr>`
-    })
-    document.getElementById("policies").innerHTML += out;
-})
+
+                    document.getElementById("tablebody").innerHTML = out;
+
+                })
+
+                .catch(error => {
+                    document.getElementById('error').innerHTML = "POPRAW DANE!"
+                })
+
+
+        }
+    }
+
+
+
+}
+)
 
 
 
