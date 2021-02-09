cdab1e927c3d6739e9f00a5e9a656d769eef4093
ilowski <wojtek-ilowski@wp.pl>
2021-02-09 22:52:35 +0100

    update endpoints with dto classess, add model mapper

--- src/main/java/com/controller/PolicyController.java
+++ src/main/java/com/controller/PolicyController.java
@@ -2,7 +2,9 @@ package com.controller;
 
 import com.entity.Customer;
 import com.entity.Policy;
+import com.entity.dto.PolicyBasicInfoDto;
 import com.service.PolicyService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,39 +16,42 @@ import javax.validation.Valid;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/policies")
 public class PolicyController {
 
-    @Autowired
+
     private PolicyService policyService;
+
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public PolicyController(PolicyService policyService, ModelMapper modelMapper) {
+        this.policyService = policyService;
+        this.modelMapper = modelMapper;
+    }
 
     @GetMapping
     public ResponseEntity<?> findAllPolicies() {
-        return new ResponseEntity<List<Policy>>(policyService.findAll(), HttpStatus.OK);
+        List<Policy> policies = policyService.findAll();
+        return new ResponseEntity<List<PolicyBasicInfoDto>>(policies.stream().map(policy -> modelMapper.map(policy, PolicyBasicInfoDto.class)).collect(Collectors.toList()),HttpStatus.OK);
     }
 
-    @GetMapping("/searchTwoWeeksPolicies")
-    public ResponseEntity<?> findTwoWeeksPolicies () {
-        return new ResponseEntity<List<Policy>>(policyService.findTwoWeeksPolicies(),HttpStatus.OK);
-    }
 
     @GetMapping("/search")
     public ResponseEntity<?> findByCriteria(@RequestParam(name = "criteria", required = true) String criteria,
                                             @RequestParam(name = "searchItem", required = true) String searchItem) {
-        try{
-        return new ResponseEntity<List<Policy>>(policyService.findByCriteria(criteria, searchItem), HttpStatus.OK); }
-        catch (ParseException e) {
-            return new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
-        }
+        List<Policy> policies = policyService.findByCriteria(criteria, searchItem);
+        return new ResponseEntity<List<PolicyBasicInfoDto>>(policies.stream().map(policy -> modelMapper.map(policy, PolicyBasicInfoDto.class)).collect(Collectors.toList()), HttpStatus.OK); }
 
-    }
+
 
 
     @PostMapping("/add")
-    public ResponseEntity<?> addPolicy(@RequestBody Policy policy) {
+    public ResponseEntity<?> addPolicy(@Valid @RequestBody PolicyBasicInfoDto policy) {
         try {
             policyService.addPolicy(policy);
             return new ResponseEntity<Void>(HttpStatus.CREATED);
@@ -57,14 +62,14 @@ public class PolicyController {
 
     @GetMapping("/{numberOfPolicy}")
     public ResponseEntity<?> findByNumberOfPolicy(@PathVariable String numberOfPolicy) {
-        Optional<Policy> policyOptional = policyService.findByNumberOfPolicy(numberOfPolicy);
-        if (policyOptional.isPresent()) {
-            return new ResponseEntity<>(policyOptional.get(), HttpStatus.OK);
+       PolicyBasicInfoDto policyBasicInfoDto = policyService.convertToPolicyBasicInfoDto(policyService.findByNumberOfPolicy(numberOfPolicy));
+        if (policyBasicInfoDto != null) {
+            return new ResponseEntity<>(policyBasicInfoDto, HttpStatus.OK);
         }
         return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/remove/{id}")
+    @DeleteMapping("/remove/{numberOfPolicy}")
     public ResponseEntity<?> removePolicy(@PathVariable String numberOfPolicy) {
         if (policyService.removePolicy(numberOfPolicy)) {
             return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
