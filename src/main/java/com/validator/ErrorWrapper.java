831da77ed9503336a01036220c416238af9e4c4c
ilowski <wojtek-ilowski@wp.pl>
2021-03-06 21:22:27 +0100

    change type of fieldErrors

--- src/main/java/com/validator/ErrorWrapper.java
+++ src/main/java/com/validator/ErrorWrapper.java
@@ -10,7 +10,8 @@ public class ErrorWrapper implements Serializable {
     private final String message;
     private final String description;
 
-    private List<FieldErrorDTO> fieldErrors;
+    private FieldErrorDTO fieldErrors;
+
 
     public ErrorWrapper(String message) {
         this(message, null);
@@ -21,17 +22,14 @@ public class ErrorWrapper implements Serializable {
         this.description = description;
     }
 
-    public ErrorWrapper(String message, String description, List<FieldErrorDTO> fieldErrors) {
+    public ErrorWrapper(String message, String description, FieldErrorDTO fieldErrors) {
         this.message = message;
         this.description = description;
         this.fieldErrors = fieldErrors;
     }
 
     public void add(String objectName, String field, String message) {
-        if (fieldErrors == null) {
-            fieldErrors = new ArrayList<>();
-        }
-        fieldErrors.add(new FieldErrorDTO(objectName, field, message));
+        this.fieldErrors = new FieldErrorDTO(objectName, field, message);
     }
 
     public String getMessage() {
@@ -42,7 +40,7 @@ public class ErrorWrapper implements Serializable {
         return description;
     }
 
-    public List<FieldErrorDTO> getFieldErrors() {
+    public FieldErrorDTO getFieldErrors() {
         return fieldErrors;
     }
 }
